cda33e3d43c5308478a284095a193f8c330b8e5d
Sarah Maris <sarah@sarahmaris.com>
2019-05-21 00:05:28 -0400

    Updates

---
+++ precache-manifest.9c7c8ca0c5fb8454c4b6c4f2af8e03b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b283da53cab4cb5f929151545a2432e",
+    "url": "/react-demo2/index.html"
+  },
+  {
+    "revision": "5fb3d4d2636f01d49a28",
+    "url": "/react-demo2/static/css/main.5eba7624.chunk.css"
+  },
+  {
+    "revision": "1fe41150d04baeef08b3",
+    "url": "/react-demo2/static/js/2.8c9c7ebf.chunk.js"
+  },
+  {
+    "revision": "5fb3d4d2636f01d49a28",
+    "url": "/react-demo2/static/js/main.9b10b1d1.chunk.js"
+  },
+  {
+    "revision": "5eae77d7479c9a667fc3",
+    "url": "/react-demo2/static/js/runtime~main.a731eeca.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/react-demo2/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-demo2/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-demo2/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "51adb8eb8d23044d1807e76fb6dda947",
+    "url": "/react-demo2/static/media/stacked-books.51adb8eb.jpg"
+  }
+]);
